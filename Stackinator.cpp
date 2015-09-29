a7693243d2cec5d71dac3c4a0b650ef7e2434976
Cyrill Pugh <cyrillpugh@gmail.com>
2015-09-29 15:09:47 +0300

    non-full stackinator.cpp
    В дальнейшем добавлю комментарии и правки.

---
+++ Stackinator.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+template <typename Item>
+class Stackinator
+{
+protected:
+	Item item;
+	Stackinator *next;
+public:
+	Stackinator(Item, Stackinator *);
+	~Stackinator() {};
+};
+
+template <class Item>
+Stackinator<Item>::Stackinator(Item i, Stackinator<Item> *n) : item(i), next(n) {}
+
+template <class Item>
+class Stack : public Stackinator<Item>
+{
+private:
+	Stackinator<Item> *head;
+public:
+	Stack() { head = 0 };
+	~Stack() {};
+	bool empty() { return head == 0 };
+	void push(Item i) { head = new Stackinator(i, head) };
+	Item pop() {
+		Item i = head->item; Stackinator *s = head->next;
+		delete head; head = s; return v;
+	}
+};
