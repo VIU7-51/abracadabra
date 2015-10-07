9824610023fd363c45fecbb0a9f4b261766b8582
julyakovleva <julyakovleva@users.noreply.github.com>
2015-10-07 23:58:36 +0300

    Create Stack(Julia).cpp

---
+++ Stack(Julia).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdlib.h>
+
+template <typename T>
+struct Node {
+	T item;
+	Node *next;
+};
+
+template <typename T>
+class Stack {
+public:
+	bool isEmpty(){
+		return first == NULL;
+	}
+	void push(T item){
+		Node<T>* old = first;
+		first = new Node<T>();
+		first->item = item;
+		first->next = old;
+	}
+	T pop(){
+		Node<T> current = *first;
+		delete first;
+		T item = current.item;
+		first = current.next;
+		return item;
+	}
+private:
+	Node<T> *first = NULL;
+};
+
+int main() {
+	Stack<char> s;
+	for (char i = 'a'; i <= 'f'; i++) 
+		s.push(i);
+	for (;!s.isEmpty();)
+		std::cout<<s.pop()<<"\n";
+	return 0;
+}
