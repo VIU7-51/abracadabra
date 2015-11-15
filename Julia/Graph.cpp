407ecdf15a8f3cda2a9a09a9a917b4545169f04d
julyakovleva <sarah-engels@yandex.ru>
2015-11-15 17:16:41 +0300

    Code is more readable now

--- Julia/Graph.cpp
+++ Julia/Graph.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <cstdlib>
 #include <list>
+#include <assert.h>
 using namespace std;
 
 #define nameOfLink iterLinks->first
@@ -12,40 +13,44 @@ using namespace std;
 #define firstLink (*iterVertex)->link.begin()
 #define lastLink (*iterVertex)->link.end()
 #define nameOfVertex (*iterVertex)->name
+#define numberOfLinks (*iterVertex)->link.size()
 #define INFINITY 1000
+typedef int Array;
 
 class Vertex {
 public:
-    Vertex() { createLinks(); }
+    Vertex();
     int name;
     map<int, int> link;
 private:
-    void createLinks() {
-        int randomWeight;
-        int randomVertex;
-        for (int randNumOfLinks = 0; randNumOfLinks<(1+rand()%3); randNumOfLinks++) {
-            randomVertex = 1+rand()%5;
-            randomWeight = 5+rand()%5;
-            if (randomVertex == name) randomVertex = 1+rand()%5;
-            link.insert(pair<int, int>(randomVertex,randomWeight));
-        }
-    }
+    void createLinks();
 };
+
+Vertex::Vertex(){
+    createLinks();
+}
+
+void Vertex::createLinks(){
+    int randomWeight;
+    int randomVertex;
+    for (int randNumOfLinks = 0; randNumOfLinks<(1+rand()%3); randNumOfLinks++) {
+        randomVertex = 1+rand()%5;
+        randomWeight = 5+rand()%5;
+        if (randomVertex == name) randomVertex = 1+rand()%5;
+        link.insert(pair<int, int>(randomVertex,randomWeight));
+    }
+}
+
 class Graph {
 private:
     int cost, step, size;
     vector<Vertex*> vertices;
     vector<Vertex*>::iterator iterVertex;
     map<int, int>::iterator iterLinks;
-    void createVert() {
-        Vertex *newVer = new Vertex;
-        for (int i = 1; i<=5; i++) {
-            newVer->name = i;
-            vertices.push_back(newVer);
-            newVer = new Vertex;
-        }
-    }
-
+    void createVert();
+    inline bool hasVertexBeenSeen(list<int>, int);
+    inline void fillArray(int*, int);
+    inline void printPathsAndCosts(int, Array*, Array*);
 public:
     Graph();
     void GreedyAlg(int startOfPath, int endOfPath);
@@ -59,53 +64,85 @@ Graph::Graph() {
     cost = 0;
     step = 0;
 }
-void Graph::Dijkstra(int start){
-    list<Vertex*> unseenVertices;
-    list<Vertex*>::iterator iterVertex;
-    list<int> seen;
+
+void Graph::createVert(){
+    Vertex *newVer = new Vertex;
+    for (int i = 1; i<=5; i++){
+        newVer->name = i;
+        vertices.push_back(newVer);
+        newVer = new Vertex;
+    }
+}
+inline bool Graph::hasVertexBeenSeen(list<int> SV, int name){
+    bool flag = false;
     list<int>::iterator iterSeen;
-    int costs[size], paths[size];
-    int weight;
+    iterSeen = SV.begin();
+    for(int i = 0; i<SV.size(); i++){
+        if(*iterSeen == name) flag = true;
+        iterSeen++;
+    }
+    return flag;
+}
+
+inline void Graph::fillArray(int *array, int value){
     for(int i = 0; i!= vertices.size();i++) {
-        costs[i]=INFINITY;
-        paths[i]=0;
+        array[i]=value;
     }
-    costs[start-1]=0;
-    paths[start-1]=start;
-    unseenVertices.push_back(vertices[start-1]);
-    iterVertex = unseenVertices.begin();
-    while(!unseenVertices.empty()){
-        if(costs[nameOfVertex-1]== INFINITY) continue;
-        seen.push_back(nameOfVertex);
-        weight = costs[nameOfVertex-1];
-        iterLinks = firstLink;
-        for (int j = 0; j < (*iterVertex)->link.size();j++){
-            if(start == nameOfLink) {iterLinks++;continue;}
-            if(costs[nameOfLink-1]>weight+weightOfLink){
-                costs[nameOfLink-1]=weight+weightOfLink;
-                paths[nameOfLink-1]=nameOfVertex;
-            }
-            bool flag = false;
-            iterSeen = seen.begin();
-            for(int i = 0; i<seen.size(); i++){
-                if(*iterSeen == nameOfLink) flag = true;
-                iterSeen++;
-            }
-            if(!flag) {
-                unseenVertices.push_back(vertices[nameOfLink-1]);
-            }
-            iterLinks++;
-        }
-        unseenVertices.erase(iterVertex);
-        iterVertex = unseenVertices.begin();
-    }
+}
+
+inline void Graph::printPathsAndCosts(int start, Array *paths, Array *costs){
     for(int i = 0; i<size;i++){
         cout<<"From "<<start<<" to "<<i+1<<": cost: "
         <<costs[i]<<" path through: "<<paths[i]<<endl;
     }
 }
 
+void Graph::Dijkstra(int start){
+    assert(start<=size);
+
+    list<Vertex*> unseenVertices;
+    list<Vertex*>::iterator iterVertex;
+    list<int> seenVertices;
+    int weight;
+    Array costs[size], paths[size];
+
+
+    fillArray(costs, INFINITY);
+    fillArray(paths, 0);
+    costs[start-1]=0;
+    paths[start-1]=start;
+
+    unseenVertices.push_back(vertices[start-1]);
+    iterVertex = unseenVertices.begin();
+    while(!unseenVertices.empty()){
+        seenVertices.push_back(nameOfVertex);
+        weight = costs[nameOfVertex-1];
+
+        iterLinks = firstLink;
+        for (int i = 0; i < numberOfLinks; i++){
+            if(start == nameOfLink){
+                iterLinks++;
+                continue;
+            }
+            if(costs[nameOfLink-1]>weight+weightOfLink){
+                costs[nameOfLink-1]=weight+weightOfLink;
+                paths[nameOfLink-1]=nameOfVertex;
+            }
+            if(!hasVertexBeenSeen(seenVertices, nameOfLink)){
+                unseenVertices.push_back(vertices[nameOfLink-1]);
+            }
+            iterLinks++;
+        }
+
+        unseenVertices.erase(iterVertex);
+        iterVertex = unseenVertices.begin();
+    }
+    printPathsAndCosts(start, paths, costs);
+}
+
 void Graph::GreedyAlg(int startOfPath, int endOfPath){
+    assert(startOfPath < size && endOfPath < size);
+
     step++;
     for (iterVertex = firstVertex; iterVertex != lastVertex; iterVertex++)
         if (nameOfVertex == startOfPath) break;
